53019137edb1470ca7daa95ebb909be8c8f0cea0
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-16 11:53:16 -0500

    Added ZOAU and the ability to dynamically get LPAR and system name so the user knows which system they're on

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from textual.app import App
 from textual.widgets import Header, Footer, Tabs, ListView, Input, Label, Tab
+from zoautil_py import zsystem
+import json
 
-system_name = "TEST"
+zsystem_info = json.loads(zsystem.zinfo())
+
+system_name = zsystem_info["sys_info"]["sys_name"]
+lpar_name = zsystem_info["sys_info"]["lpar_name"]
 
 class Blackwall(App):
     #This portion handles the text in the header bar
@@ -10,7 +15,7 @@ class Blackwall(App):
         self.sub_title = "Mainframe Security Administration"
 
     def compose(self):
-        yield Label(f"You are working on mainframe system {system_name}")
+        yield Label(f"You are working on the {system_name} mainframe system in LPAR {lpar_name}")
         yield Input()
         yield Header()
         yield Tabs(
