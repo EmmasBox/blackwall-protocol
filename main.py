f11dbf46847a964e090373e0a0f6d380b62b91e9
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-16 12:19:40 -0500

    added more comments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from textual.widgets import Header, Footer, Tabs, ListView, Input, Label, Tab
 from zoautil_py import zsystem
 import json
 
+#system information
 zsystem_info = json.loads(zsystem.zinfo())
-
 system_name = zsystem_info["sys_info"]["sys_name"]
 lpar_name = zsystem_info["sys_info"]["lpar_name"]
 
@@ -15,6 +15,7 @@ class Blackwall(App):
         self.sub_title = "Mainframe Security Administration"
 
     def compose(self):
+        #display system and LPAR name
         yield Label(f"You are working on the {system_name} mainframe system in LPAR {lpar_name}")
         yield Input()
         yield Header()
