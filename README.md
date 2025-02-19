5a85520327b4060da7f930b29a5afd04cb185faf
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-02-19 12:45:53 +0100

    Made ZOAU an optional dependency

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Blackwall Protocol
 Blackwall Protocol is a terminal based administration panel for RACF. The goal of Blackwall is to create a more modern and open source alternative to zSecure.
 
-Dependencies:
+Required dependencies:
 - Python 3.11 or later
-- textual (For UI)
-- ZOAU (For gathering system information like LPAR name)
+- Textual (For UI)
 - RACFU (To communicate with RACF)
+
+Optional dependencies:
+- ZOAU (For gathering system information like LPAR name, not required but highly recommended)
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from textual.app import App
 from textual.widgets import Header, Footer, Tabs, ListView, Input, Label, Tab
-from zoautil_py import zsystem
+try:
+    from zoautil_py import zsystem
+    zoau_enabled = True
+except:
+    zoau_enabled = False
+
 import json
 
 #system information
-zsystem_info = json.loads(zsystem.zinfo())
-system_name = zsystem_info["sys_info"]["sys_name"]
-lpar_name = zsystem_info["sys_info"]["lpar_name"]
+if zoau_enabled:
+    zsystem_info = json.loads(zsystem.zinfo())
+    system_name = zsystem_info["sys_info"]["sys_name"]
+    lpar_name = zsystem_info["sys_info"]["lpar_name"]
 
 class Blackwall(App):
     #This portion handles the text in the header bar
@@ -16,7 +22,8 @@ class Blackwall(App):
 
     def compose(self):
         #display system and LPAR name
-        yield Label(f"You are working on the {system_name} mainframe system in LPAR {lpar_name}")
+        if zoau_enabled:
+            yield Label(f"You are working on the {system_name} mainframe system in LPAR {lpar_name}")
         yield Input()
         yield Header()
         yield Tabs(
