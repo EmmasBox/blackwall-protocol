f56979ab71e3d249e1f1c42d5cc0e4558330d1a6
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-26 11:18:26 +0100

    Fixed missing self

--- src/API.py
+++ src/API.py
@@ -11,7 +11,7 @@ except:
 class RACFAPI():
     if racfu_enabled:
         #User functions
-        def user_exists(username):
+        def user_exists(self, username: str):
             """Checks if a user exists, returns true or false"""
             result = racfu({"operation": "extract", "admin_type": "user", "profile_name": {username}})
             if result.result["return_codes"]["racf_return_code"] == "0":
@@ -19,48 +19,48 @@ class RACFAPI():
             else:
                 return False
 
-        def user_get():
+        def user_get(self):
             pass
 
-        def user_create():
+        def user_create(self):
             pass
 
-        def user_delete():
+        def user_delete(self):
             pass
 
-        def user_update():
+        def user_update(self):
             pass
 
         #Dataset functions
-        def dataset_profile_exists():
+        def dataset_profile_exists(self):
             """Checks if a dataset profile exists, returns true or false"""
             pass
 
-        def dataset_profile_get():
+        def dataset_profile_get(self):
             pass
 
-        def dataset_profile_create():
+        def dataset_profile_create(self):
             pass
 
-        def dataset_profile_delete():
+        def dataset_profile_delete(self):
             pass
 
-        def dataset_profile_update():
+        def dataset_profile_update(self):
             pass
 
         #General resource profile function
-        def resource_profile_exists():
+        def resource_profile_exists(self):
             """Checks if a general resource profile exists, returns true or false"""
             pass
 
-        def resource_profile_get():
+        def resource_profile_get(self):
             pass
 
-        def resource_profile_create():
+        def resource_profile_create(self):
             pass
 
-        def resource_profile_delete():
+        def resource_profile_delete(self):
             pass
 
-        def resource_profile_update():
+        def resource_profile_update(self):
             pass
