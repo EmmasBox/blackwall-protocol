f4a987de663b0b3e1e6d43b5b25030f40447a96f
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-26 17:08:19 +0100

    Finished more segments in API_user

--- src/API_user.py
+++ src/API_user.py
@@ -13,79 +13,125 @@ except:
 
 @dataclass
 class BaseUserTraits(TraitsBase):
+    #primary
     owner: str
     default_group: str
     name: str | None = None
+    installation_data: str | None = None
+
+    #user attributes
+    special: str | None = None 
+    operations: str | None = None 
+    auditor: str | None = None
+    
+    default_group_authority: str | None = None
+    security_category: str | None = None
+    security_categories: str | None = None
+    class_authorization: str | None = None
 
 @dataclass
 class CICSUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    operator_class: str | None = None
+    operator_id: str | None = None
+    operator_priority: str | None = None
+    resource_security_level_key: str | None = None
+    resource_security_level_keys: str | None = None
+    timeout: str | None = None
+    transaction_security_level_key: str | None = None
+    force_signoff_when_xrf_takeover: bool | None = None
 
 @dataclass
 class DCEUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    auto_login: bool | None = None
+    name: str | None = None
+    home_cell: str | None = None
+    home_cell_uuid: str | None = None
+    uuid: str | None = None
 
 @dataclass
 class DFPUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    data_application: str | None = None
+    data_class: str | None = None
+    management_class: str | None = None
+    storage_class: str | None = None
 
 @dataclass
 class EIMUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    ldap_bind_profile: str | None = None
 
 @dataclass
 class KerbUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    encryption_algorithm: str | None = None
+    name: str | None = None
+    key_from: str | None = None
+    key_version: str | None = None
+    max_ticket_life: str | None = None
 
 @dataclass
 class LanguageUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    primary: str | None = None
+    secondary: str | None = None
 
 @dataclass
 class LnotesUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    zos_short_name: str | None = None
 
 @dataclass
 class MfaUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    factor: str | None = None
+    active: bool | None = None
+    tags: str | None = None
+    password_fallback: bool | None = None
+    mfa_policy: str | None = None
 
 @dataclass
 class NDSUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    username: str | None = None
 
 @dataclass
 class NetviewUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    default_mcs_console_name: str | None = None
+    security_control_check: str | None = None
+    domain: str | None = None
+    logon_commands: str | None = None
+    receive_unsolicited_messages: str | None = None
+    operator_graphic_monitor_facility_administration_allowed: str | None = None
+    operator_graphic_monitor_facility_display_authority: str | None = None
+    operator_scope_classes: str | None = None
 
 @dataclass
 class OMVSUserTraits(TraitsBase):
     uid: str | None = None
     home_directory: str | None = None
+    auto_uid: bool | None = None
+    max_address_space_size: int | None = None
+    max_cpu_time: int | None = None
+    max_files_per_process: int | None = None
+    home_directory: str | None = None
+    max_non_shared_memory: str | None = None
+    max_file_mapping_pages: int | None = None
+    max_processes: int | None = None
+    default_shell: str | None = None
+    shared: bool | None = None
+    max_shared_memory: int | None = None
+    max_threads: int | None = None
 
 @dataclass
 class OperparmUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    pass
 
 @dataclass
 class OvmUserTraits(TraitsBase):
-    uid: str | None = None
+    file_system_root: str | None = None
     home_directory: str | None = None
+    default_shell: str | None = None
+    uid: str | None = None
 
 @dataclass
 class ProxyUserTraits(TraitsBase):
-    uid: str | None = None
-    home_directory: str | None = None
+    bind_distinguished_name: str | None = None
+    bind_password: str | None = None
+    ldap_host: str | None = None
 
 @dataclass
 class TSOUserTraits(TraitsBase):
