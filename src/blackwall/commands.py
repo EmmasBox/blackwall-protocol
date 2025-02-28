de0302fbd96096752e7ed82d0373a3cee4e280d2
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-28 16:36:42 +0100

    fixed log history

--- src/blackwall/commands.py
+++ src/blackwall/commands.py
@@ -8,12 +8,10 @@ from textual.screen import Screen
 
 import subprocess
 
-command_history = ""
-
 class CommandHistoryScreen(Screen):
     def compose(self) -> ComposeResult:
         yield Label("Command history: ")
-        yield Log(command_history)
+        yield Log()
 
 class MVSCommandField(HorizontalGroup):
     def compose(self) -> ComposeResult:
@@ -21,12 +19,12 @@ class MVSCommandField(HorizontalGroup):
 
     @on(Input.Submitted)
     def execute_command(self) -> None:
-        global command_history
         command = self.query_exactly_one(selector="#cli").value
         if command != "":
             try:
                 output = subprocess.run(f'tsocmd "{command}"' , shell=True, check=True, capture_output=True,encoding="utf-8")
-                command_history = command_history + output.stdout
+                log = self.parent.query_one(Log)
+                log.write(output.stdout)
                 self.notify(f"command {command.upper()} successfully completed",severity="information")
             except BaseException as e:
                 self.notify(f"Command {command.upper()} failed: {e}",severity="error")
