512fe2c2352e644978e6c0fe4bc794166f424c3e
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-28 16:10:06 +0100

    screen test

--- src/blackwall/commands.py
+++ src/blackwall/commands.py
@@ -2,10 +2,18 @@ from . import commands_definition as defintion
 from textual.app import ComposeResult
 
 from textual import on
-from textual.widgets import Input
+from textual.widgets import Input, Log, Label
 from textual.containers import HorizontalGroup
+from textual.screen import Screen
 
 import subprocess
+
+class CommandOutputScreen(Screen):
+    BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
+
+    def compose(self) -> ComposeResult:
+        yield Label()
+        yield Log()
 
 class MVSCommandField(HorizontalGroup):
     def compose(self) -> ComposeResult:
@@ -18,5 +26,6 @@ class MVSCommandField(HorizontalGroup):
             try:
                 output = subprocess.run(f'tsocmd "{command}"' , shell=True, check=True, capture_output=True)
                 self.notify(f"command {command.upper()} successfully completed",severity="information")
+                self.install_screen(CommandOutputScreen(), name="bsod")
             except:
                 self.notify(f"Command {command.upper()} failed",severity="error")
