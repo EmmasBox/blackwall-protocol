477ce26f6ba7bf57bfd3add6786b5e6802ac3cf9
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-02-28 17:05:36 +0100

    updated header

--- src/blackwall/commands.py
+++ src/blackwall/commands.py
@@ -15,13 +15,14 @@ command_history = ""
 
 
 
-def generate_command_meta(command):
+def generate_command_meta_header(command):
     now = datetime.now() # current date and time
     date_time = now.strftime("d-%m-%d-%Y-t-%H-%M-%S")
     return f"""
-    ------------------------------------------------------
-    Command '{command}' executed on {date_time}
-    ------------------------------------------------------
+    --------------------------------------------------------------------------------------------------
+    Command '{command}' 
+    executed on {date_time}
+    --------------------------------------------------------------------------------------------------
     \n
     """
 
@@ -50,7 +51,7 @@ class MVSCommandField(HorizontalGroup):
         if command != "":
             try:
                 output = subprocess.run(f'tsocmd "{command}"' , shell=True, check=True, capture_output=True,encoding="utf-8")
-                command_history = command_history + generate_command_meta(command) + output.stdout
+                command_history = command_history + generate_command_meta_header(command) + output.stdout
                 self.notify(f"command {command.upper()} successfully completed",severity="information")
             except BaseException as e:
                 self.notify(f"Command {command.upper()} failed: {e}",severity="error")
