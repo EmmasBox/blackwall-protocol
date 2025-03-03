54cda10bb96461e280ab99d0d3a1bec1f056e35f
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-03 14:52:23 +0100

    Updated health check options

--- src/blackwall/panel_analysis.py
+++ src/blackwall/panel_analysis.py
@@ -3,14 +3,21 @@ from textual.app import ComposeResult
 from textual.widgets import Button, Label, RadioButton, RadioSet, Log
 from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll, Horizontal
 
+try:
+    from zoautil_py import zsystem # type: ignore
+    zoau_enabled = True
+except:
+    print("##BLKWL_ERROR_1 Warning: could not find ZOAU, disabling APF health checks")    
+    zoau_enabled = False
+
 class AnalysisSelector(VerticalGroup):
     def compose(self) -> ComposeResult:
         yield Label("Select desired health checks: ")
         with RadioSet(id="selector-health-checks",classes="selector-health-checks"):
-            yield RadioButton("All",value=True)
-            yield RadioButton("z/OS Unix excessive permissions")
-            yield RadioButton("Unprotected APF datasets")
-            yield RadioButton("Unused users")
+            yield RadioButton("UACC best practice",value=True, tooltip="Checks if UACC is being used instead of any=*")
+            yield RadioButton("z/OS Unix excessive permissions",tooltip="Checks if files have RWX set for everyone on the system")
+            yield RadioButton("Unprotected APF datasets",tooltip="Checks if APF datasets are unprotected by a dataset profile")
+            yield RadioButton("Unused users","Checks if users haven't been used in a really long time")
 
 class AnalysisLog(VerticalGroup):
     def compose(self) -> ComposeResult:
