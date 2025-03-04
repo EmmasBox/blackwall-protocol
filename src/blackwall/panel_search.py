83e4f3f0b7eb42e6040ebfc8955d9cecf918217c
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-04 15:46:01 +0100

    Removed option

--- src/blackwall/panel_search.py
+++ src/blackwall/panel_search.py
@@ -13,10 +13,6 @@ class SearchSelector(HorizontalGroup):
         with RadioSet(id="filter-selector",classes="search-selector"):
             yield RadioButton("All",value=True)
             yield RadioButton("Only one")
-        with RadioSet(id="where-selector",classes="search-selector"):
-            yield RadioButton("Occurence of",value=True)
-            yield RadioButton("Occurence in")
-
 
 class SearchField(HorizontalGroup):
     def compose(self) -> ComposeResult:
