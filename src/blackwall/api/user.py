36a2ebe7ca0ba7f367f940df0c6b217722d51237
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-03-12 12:41:28 +0100

    added debug thing

--- src/blackwall/api/user.py
+++ src/blackwall/api/user.py
@@ -271,6 +271,7 @@ if racfu_enabled:
                     "traits":  traits
                 }
             )
+        print(result.result["return_codes"])
         return result.result["return_codes"]["racf_return_code"] == "0" 
 
     def user_delete(username: str):
