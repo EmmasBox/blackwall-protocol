92871af921147c878c55cde51305c867137cb1ab
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-14 14:39:14 +0100

    fixed bug

--- src/blackwall/panels/users/user.py
+++ src/blackwall/panels/users/user.py
@@ -168,7 +168,7 @@ class PanelUser(VerticalScroll):
         user_name_panel.mode = PanelMode.edit
         self.query_exactly_one(selector="#username").disabled = True
         self.query_exactly_one(selector="#delete").disabled = False
-        self.query_exactly_one(selector="#save").name = "Save"
+        self.query_exactly_one(selector="#save").label = "Save"
         self.notify(f"Switched to edit mode",severity="information")
 
     def action_delete_user(self) -> None:
