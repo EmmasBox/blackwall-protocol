85d915c8350c19c6de07f7ae3f3c6c6f0abe5b3b
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-15 19:14:34 +0100

    Added installation data field to the dataset profile management panel

--- src/blackwall/panels/dataset/dataset.py
+++ src/blackwall/panels/dataset/dataset.py
@@ -4,22 +4,27 @@ from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll, H
 
 class PanelDatasetName(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Label("Profile name")
+        yield Label("Profile name:")
         yield Input()
+
+class PanelDatasetInstallationData(VerticalGroup):
+    def compose(self) -> ComposeResult:
+        yield Label("Installation data:")
+        yield Input(max_length=100,id="installation_data",classes="installation-data",tooltip="Installation data is an optional piece of data you can assign to a dataset profile. You can use installation data to describe whatever you want, such as owning department or what kind of data it protects")
 
 class PanelDatasetAudit(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Label("Audit")
+        yield Label("Audit:")
         yield Select([("ALL", 1),("FAILURES", 2),("NONE", 3),("SUCCESS", 4)],value=2,classes="uacc-select")
 
 class PanelDatasetUACC(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Label("UACC")
+        yield Label("UACC:")
         yield Select([("NONE", 1),("READ", 2),("EXECUTE", 3),("UPDATE", 4),("CONTROL", 5),("ALTER", 6)],value=1,classes="uacc-select")
 
 class PanelDatasetNotify(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Label("Notify user")
+        yield Label("Notify user:")
         yield Input() 
 
 class PanelDatasetAccessSettings(HorizontalGroup):
@@ -36,5 +41,6 @@ class PanelDatasetActionButtons(HorizontalGroup):
 class PanelDataset(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield PanelDatasetName()
+        yield PanelDatasetInstallationData()
         yield PanelDatasetAccessSettings()
         yield PanelDatasetActionButtons()
