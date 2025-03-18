b2d42e190ceec56ec3cb0187987d7c9fe0c1b21c
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-18 16:11:01 +0100

    Resource API can now delete resources

--- src/blackwall/api/resource.py
+++ src/blackwall/api/resource.py
@@ -13,21 +13,26 @@ except:
 
 if racfu_enabled:
     #General resource profile function
-    def resource_profile_exists(resource: str):
+    def resource_profile_exists(resource_class: str,resource: str):
         """Checks if a general resource profile exists, returns true or false"""
         result = racfu({"operation": "extract", "admin_type": "resource", "profile_name": resource})
         return result.result["return_codes"]["racf_return_code"] == "0"
 
-    def resource_profile_get(resource: str):
+    def resource_profile_get(resource_class: str,resource: str):
         """Doesn't handle general resource profiles that don't exist, recommend using resource_profile_exists() first"""
         result = racfu({"operation": "extract", "admin_type": "resource", "profile_name": resource})
         return result.result
 
-    def update_resource_profile():
+    def update_resource_profile(resource_class: str,resource: str):
         pass
 
-    def delete_resource_profile(resource: str):
-        pass
-
-    def update_resource_profile(resource: str):
-        pass
+    def delete_resource_profile(resource_class: str,resource: str):
+        result = racfu(
+                {
+                    "operation": "delete", 
+                    "admin_type": "resource", 
+                    "profile_name": resource,
+                    "class_name": resource_class
+                }
+            )
+        return result.result["return_codes"]["racf_return_code"] == 0
