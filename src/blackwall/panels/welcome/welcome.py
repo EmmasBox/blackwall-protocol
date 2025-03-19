714d35fa05cbcfd22cba9f5e4656976bae6a5e9f
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-19 21:29:40 +0100

    Further work on the welcome page

--- src/blackwall/panels/welcome/welcome.py
+++ src/blackwall/panels/welcome/welcome.py
@@ -6,6 +6,23 @@ from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 from importlib.resources import files
 message = files('blackwall.panels.welcome').joinpath('welcome_message.md').read_text()
 
+class PanelWelcomeMessage(VerticalGroup):
+    def compose(self) -> ComposeResult:
+        yield Markdown(message,classes="welcome-message")
+
+class PanelWelcomeActions(VerticalGroup):
+    def compose(self) -> ComposeResult:
+        yield Label("Try out the program:",classes="welcome-suggestion-header")
+        yield Button("Create user", classes="welcome-suggestion-button")
+        yield Button("Create dataset profile", classes="welcome-suggestion-button")
+        yield Button("Create general resource profile", classes="welcome-suggestion-button")
+        yield Button("Analyse system health", classes="welcome-suggestion-button")
+
+class PanelWelcomeMain(HorizontalGroup):
+    def compose(self) -> ComposeResult:
+        yield PanelWelcomeMessage()
+        yield PanelWelcomeActions()
+
 class PanelWelcome(VerticalScroll):
     def compose(self) -> ComposeResult:
-        yield Markdown(message)
+        yield PanelWelcomeMain()
