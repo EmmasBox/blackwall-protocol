255e44f2bc657e65f279fca3302d2c5a72fd0c1c
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-23 17:55:26 +0100

    fixed missing css class

--- src/blackwall/api/user.py
+++ src/blackwall/api/user.py
@@ -116,7 +116,7 @@ class OMVSUserTraits(TraitsBase):
     shared: bool | None = field(default=None, metadata={"label": "shared"})
     home_directory: str | None = field(default=None, metadata={"label": "home directory", "input_args": {"classes": "field-long-generic"}})
     default_shell: str | None = field(default=None, metadata={"label": "default shell", "input_args": {"classes": "field-long-generic"}})
-    max_address_space_size: int | None = field(default=None, metadata={"label": "max address space size"})
+    max_address_space_size: int | None = field(default=None, metadata={"label": "max address space size", "input_args": {"classes": "field-long-generic"}})
     max_cpu_time: int | None = field(default=None, metadata={"label": "home directory", "input_args": {"max_length": 10, "classes": "field-medium-generic"}})
     max_files_per_process: int | None = field(default=None, metadata={"label": "max files per process", "input_args": {"classes": "field-long-generic"}})
     max_file_mapping_pages: int | None = field(default=None, metadata={"label": "max file mapping pages", "input_args": {"classes": "field-long-generic"}})
