15f2fa911b88c4a3fceee88a7671ed156bed81f0
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-23 18:09:31 +0100

    Added css classes to the kerb fields

--- src/blackwall/api/user.py
+++ src/blackwall/api/user.py
@@ -71,11 +71,11 @@ class EIMUserTraits(TraitsBase):
 
 @dataclass
 class KerbUserTraits(TraitsBase):
-    encryption_algorithm: str | None = field(default=None, metadata={"label": "encryption algorithm", "input_args": {"max_length": 255}})
-    name: str | None = field(default=None, metadata={"label": "name", "input_args": {"max_length": 255}})
-    key_from: str | None = field(default=None, metadata={"label": "key from", "input_args": {"max_length": 8}})
-    key_version: str | None = field(default=None, metadata={"label": "key version", "input_args": {"max_length": 3}})
-    max_ticket_life: int | None = field(default=None, metadata={"label": "max ticket life", "input_args": {"max_length": 16}})
+    encryption_algorithm: str | None = field(default=None, metadata={"label": "encryption algorithm", "input_args": {"max_length": 255,"classes": "field-long-generic"}})
+    name: str | None = field(default=None, metadata={"label": "name", "input_args": {"max_length": 255,"classes": "field-long-generic"}})
+    key_from: str | None = field(default=None, metadata={"label": "key from", "input_args": {"max_length": 8,"classes": "field-short-generic"}})
+    key_version: str | None = field(default=None, metadata={"label": "key version", "input_args": {"max_length": 3,"classes": "field-short-generic"}})
+    max_ticket_life: int | None = field(default=None, metadata={"label": "max ticket life", "input_args": {"max_length": 16,"classes": "field-medium-generic"}})
 
 @dataclass
 class LanguageUserTraits(TraitsBase):
