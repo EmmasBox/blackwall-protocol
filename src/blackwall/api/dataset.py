d6b166398d5ca4cb60967eff7844c35c751a0018
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-25 18:38:09 +0100

    Added fields to the dataset API

--- src/blackwall/api/dataset.py
+++ src/blackwall/api/dataset.py
@@ -1,6 +1,6 @@
 #API module for Blackwall Protocol, this wraps RACFU to increase ease of use and prevent updates from borking everything
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from .traits_base import TraitsBase
 
 #Checks if RACFU can be imported
@@ -13,39 +13,39 @@ except:
 
 @dataclass
 class BaseDatasetTraits(TraitsBase):
-    owner: str
-    audit_alter: str | None = None
-    audit_control: str | None = None
-    audit_none: str | None = None
-    audit_read: str | None = None
-    audit_read: str | None = None
-    audit_update: str | None = None
-    security_category: str | None = None
-    installation_data: str | None = None
-    data_set_type: str | None = None
-    erase_data_sets_on_delete: bool | None = None
-    model_profile_class: str | None = None
-    model_profile_generic: str | None = None
-    tape_data_set_file_sequence_number: int | None = None
-    model_profile: str | None = None
-    model_profile_volume: str | None = None
-    global_audit_alter: str | None = None
-    global_audit_control: str | None = None
-    global_audit_none: str | None = None
-    global_audit_read: str | None = None
-    global_audit_update: str | None = None
-    level: int | None = None
-    data_set_model_profile: str | None = None
-    notify_userid: str | None = None
-    auditing: str | None = None
-    security_label: str | None = None
-    security_level: str | None = None
-    racf_indicated_dataset: str | None = None
-    create_only_tape_vtoc_entry: str | None = None
-    universal_access: str | None = None
-    data_set_allocation_unit: str | None = None
-    volume: str | None = None
-    warn_on_insufficient_access: str | None = None
+    owner: str | None = field(default=None)
+    audit_alter: str | None = field(default=None)
+    audit_control: str | None = field(default=None)
+    audit_none: str | None = field(default=None)
+    audit_read: str | None = field(default=None)
+    audit_read: str | None = field(default=None)
+    audit_update: str | None = field(default=None)
+    security_category: str | None = field(default=None)
+    installation_data: str | None = field(default=None)
+    data_set_type: str | None = field(default=None)
+    erase_data_sets_on_delete: bool | None = field(default=None)
+    model_profile_class: str | None = field(default=None)
+    model_profile_generic: str | None = field(default=None)
+    tape_data_set_file_sequence_number: int | None = field(default=None)
+    model_profile: str | None = field(default=None)
+    model_profile_volume: str | None = field(default=None)
+    global_audit_alter: str | None = field(default=None)
+    global_audit_control: str | None = field(default=None)
+    global_audit_none: str | None = field(default=None)
+    global_audit_read: str | None = field(default=None)
+    global_audit_update: str | None = field(default=None)
+    level: int | None = field(default=None)
+    data_set_model_profile: str | None = field(default=None)
+    notify_userid: str | None = field(default=None)
+    auditing: str | None = field(default=None)
+    security_label: str | None = field(default=None)
+    security_level: str | None = field(default=None)
+    racf_indicated_dataset: str | None = field(default=None)
+    create_only_tape_vtoc_entry: str | None = field(default=None)
+    universal_access: str | None = field(default=None)
+    data_set_allocation_unit: str | None = field(default=None)
+    volume: str | None = field(default=None)
+    warn_on_insufficient_access: str | None = field(default=None)
 
 #Checks if RACFU can be imported
 try:
