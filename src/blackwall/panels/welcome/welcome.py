bf0a32556f8e3a2e234da86de8b107334964e973
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-25 21:07:11 +0100

    Removed unnecessary label

--- src/blackwall/panels/welcome/welcome.py
+++ src/blackwall/panels/welcome/welcome.py
@@ -13,10 +13,23 @@ class PanelWelcomeMessage(VerticalGroup):
 class PanelWelcomeActions(VerticalGroup):
     def compose(self) -> ComposeResult:
         yield Label("Try out the program:",classes="welcome-suggestion-header")
-        yield Button("Create user", classes="welcome-suggestion-button")
-        yield Button("Create dataset profile", classes="welcome-suggestion-button")
-        yield Button("Create general resource profile", classes="welcome-suggestion-button")
-        yield Button("Analyse system health", classes="welcome-suggestion-button")
+        yield Button("Create user", classes="welcome-suggestion-button",action="create_user")
+        yield Button("Create dataset profile", classes="welcome-suggestion-button",action="create_dataset")
+        yield Button("Create general resource profile", classes="welcome-suggestion-button",action="create_resource")
+        yield Button("Analyse system health", classes="welcome-suggestion-button",action="create_analysis")
+
+    async def action_create_dataset(self):
+        pass
+
+    async def action_create_resource(self):
+        pass
+    
+    async def action_create_user(self):
+        pass
+
+    async def action_create_analysis(self):
+        pass
+
 
 class PanelWelcomeMain(HorizontalGroup):
     def compose(self) -> ComposeResult:
