c5fc700fdfbf659ec94be936ea1bb1edffb20927
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-26 18:43:17 +0100

    Cleaned up code and reorganized things

--- src/blackwall/panels/users/user.py
+++ src/blackwall/panels/users/user.py
@@ -13,6 +13,8 @@ from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 from blackwall.api import user
 from blackwall.panels.panel_mode import PanelMode
 
+from ..traits_ui import generate_trait_inputs, get_traits_from_input
+
 class PanelUserInfo(HorizontalGroup):
     edit_mode: reactive[PanelMode] = reactive(PanelMode.create,recompose=True)
 
@@ -99,42 +101,6 @@ class PanelUserDatasetsAndUACC(VerticalGroup):
             yield Label("Group dataset access:")
             yield Input(max_length=8,id="base_group_data_set_access",classes="field-short-generic")
 
-def get_actual(field: Field) -> tuple[type,bool]:
-    # UnionType is 'str | None'
-    if isinstance(field.type, UnionType):
-        # parse out actual type out of optional type
-        # will be tuple (type(str), type(None))
-        args = get_args(field.type)
-        # the field is optional if type args contains 'type(None)'
-        optional = type(None) in args
-        # the actual type is the first non-'type(None)' in args
-        actual_type = next((t for t in args if t is not type(None)), field.type)
-    else:
-        optional = False
-        actual_type = field.type
-    return actual_type, optional
-
-def generate_trait_inputs(title: str, prefix: str, traits_class: type[user.TraitsBase]) -> Generator:
-    with Lazy(widget=Collapsible(title=title)):
-        for field in fields(traits_class):
-            label = field.metadata.get("label")
-            # only show an input field if it is labelled
-            if label is not None:
-                actual_type, optional = get_actual(field)
-
-                input_args = field.metadata.get("input_args", {})
-
-                input_id = f"{prefix}_{field.name}"
-
-                if actual_type is str:
-                    yield Label(f"{label}{'*' if not optional else ''}:")
-                    yield Input(id=input_id, **input_args)
-                elif actual_type is int:
-                    yield Label(f"{label}{'*' if not optional else ''}:")
-                    yield Input(id=input_id, type="integer", **input_args)
-                elif actual_type is bool:
-                    yield RadioButton(label=label, id=input_id, **input_args)
-
 class PanelUserSegments(VerticalGroup):
     """Component where the user can add segments such as the OMVS segment"""
     def compose(self) -> ComposeResult:
@@ -191,32 +157,6 @@ class UserInfo:
     owner: str = ""
     dfltgrp: str = ""
     installation_data: str = ""
-
-def get_traits_from_input(operator: str, widget: Widget, prefix: str, trait_cls: user.TraitsBase):
-    value = trait_cls()
-    for field in fields(trait_cls):
-        actual_type, optional = get_actual(field)
-        allowed_in = field.metadata.get("allowed_in")
-        if allowed_in is not None and operator not in allowed_in:
-            continue
-
-        input_id = f"#{prefix}_{field.name}"
-        try:
-            field_value = widget.query_exactly_one(selector=input_id).value
-        except:
-            field_value = None
-
-        if actual_type is str:
-            if field_value == "":
-                field_value = None
-        elif actual_type is int:
-            if field_value == "" or field_value == 0 or field_value is None:
-                field_value = None
-            else:
-                field_value = int(field_value)
-
-        setattr(value, field.name, field_value)
-    return value
 
 class PanelUser(VerticalScroll):
     user_info: reactive[UserInfo] = reactive(UserInfo)
