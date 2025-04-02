d559ecd09c00a3d73a99c7458d4f903f442958b5
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-02 15:58:20 +0200

    Removed lazy collapsible from lists

--- src/blackwall/panels/traits_ui.py
+++ src/blackwall/panels/traits_ui.py
@@ -42,7 +42,7 @@ def generate_trait_inputs(prefix: str, traits_class: type[TraitsBase],disabled: 
                 yield Label(f"{label}{'*' if not optional else ''}:")
                 yield Input(id=input_id, type="integer", disabled=disabled, **input_args)
             elif actual_type == list[str]:
-                with Lazy(widget=Collapsible(title=label)):
+                with Collapsible(title=label):
                     yield ListView(id=input_id, disabled=disabled, **input_args)
             elif actual_type is bool:
                 yield RadioButton(label=label, id=input_id, disabled=disabled, **input_args)
