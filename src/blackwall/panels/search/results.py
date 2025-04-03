a39ab5f4c683d733cd0e47ce99a1c44e72578728
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-03 17:08:27 +0200

    Added labels to the results panel

--- src/blackwall/panels/search/results.py
+++ src/blackwall/panels/search/results.py
@@ -13,9 +13,13 @@ DATASET_COLUMNS = [
 
 class PanelResultsMixedType(VerticalScroll):
     def compose(self) -> ComposeResult:
+        yield Label("Users:")
         yield DataTable(id="results_user_table")
+        yield Label("Groups:")
         yield DataTable(id="results_group_table")
+        yield Label("Dataset profiles:")
         yield DataTable(id="results_dataset_table")
+        yield Label("General resources profiles:")
         yield DataTable(id="results_resource_table")
 
 class PanelResultsSingleType(VerticalScroll):
