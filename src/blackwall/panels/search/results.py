9bdfe01a27a1082888c777fa983ad2b602f38b86
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-03 17:12:49 +0200

    column test

--- src/blackwall/panels/search/results.py
+++ src/blackwall/panels/search/results.py
@@ -14,13 +14,24 @@ DATASET_COLUMNS = [
 class PanelResultsMixedType(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Label("Users:")
-        yield DataTable(id="results_user_table")
+        yield DataTable(id="results_user_table",)
         yield Label("Groups:")
         yield DataTable(id="results_group_table")
         yield Label("Dataset profiles:")
         yield DataTable(id="results_dataset_table")
         yield Label("General resources profiles:")
         yield DataTable(id="results_resource_table")
+
+    def on_mount(self) -> None:
+        user_table = self.query_exactly_one(selector="#results_user_table")
+        user_table.cursor_type = next(cursors)
+        user_table.zebra_stripes = True
+        user_table.add_columns(*USER_COLUMNS[0])
+
+        dataset_table = self.query_exactly_one(selector="#results_dataset_table")
+        dataset_table.cursor_type = next(cursors)
+        dataset_table.zebra_stripes = True
+        dataset_table.add_columns(*DATASET_COLUMNS[0])
 
 class PanelResultsSingleType(VerticalScroll):
     def compose(self) -> ComposeResult:
