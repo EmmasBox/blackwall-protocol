3e96ea65fd7d54365a5f739b11aa99363949ec75
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-03 17:33:20 +0200

    Added variables in preparation for the search functionality

--- src/blackwall/panels/search/results.py
+++ src/blackwall/panels/search/results.py
@@ -16,6 +16,10 @@ DATASET_COLUMNS = [
 ]
 
 class PanelResultsUsers(VerticalScroll):
+    def __init__(self, user_dict: dict):
+        super().__init__()
+        self.user_dict = user_dict
+
     def compose(self) -> ComposeResult:
         yield Label("Users:")
         yield DataTable(id="results_user_table")     
@@ -26,6 +30,10 @@ class PanelResultsUsers(VerticalScroll):
         user_table.add_columns(*USER_COLUMNS[0])   
 
 class PanelResultsGroup(VerticalScroll):
+    def __init__(self, group_dict: dict):
+        super().__init__()
+        self.group_dict = group_dict
+
     def compose(self) -> ComposeResult:
         yield Label("Groups:")
         yield DataTable(id="results_group_table")    
@@ -36,6 +44,10 @@ class PanelResultsGroup(VerticalScroll):
         group_table.add_columns(*GROUP_COLUMNS[0])
 
 class PanelResultsDatasets(VerticalScroll):
+    def __init__(self, dataset_dict: dict):
+        super().__init__()
+        self.dataset_dict = dataset_dict
+
     def compose(self) -> ComposeResult:
         yield Label("Dataset profiles:")
         yield DataTable(id="results_dataset_table")        
@@ -46,13 +58,24 @@ class PanelResultsDatasets(VerticalScroll):
         dataset_table.add_columns(*DATASET_COLUMNS[0])
 
 class PanelResultsResources(VerticalScroll):
+    def __init__(self, resource_dict: dict):
+        super().__init__()
+        self.resource_dict = resource_dict
+
     def compose(self) -> ComposeResult:
         yield Label("General resources profiles:")
         yield DataTable(id="results_dataset_table")        
 
 class PanelResultsMixedType(VerticalScroll):
+    def __init__(self, user_dict: dict, group_dict: dict, dataset_dict: dict, resource_dict: dict):
+        super().__init__()
+        self.user_dict = user_dict
+        self.group_dict = group_dict
+        self.dataset_dict = dataset_dict
+        self.resource_dict = resource_dict
+
     def compose(self) -> ComposeResult:
-        yield PanelResultsUsers()
-        yield PanelResultsGroup()
-        yield PanelResultsDatasets()
-        yield PanelResultsResources()
+        yield PanelResultsUsers(user_dict=self.user_dict)
+        yield PanelResultsGroup(group_dict=self.group_dict)
+        yield PanelResultsDatasets(dataset_dict=self.dataset_dict)
+        yield PanelResultsResources(resource_dict=self.resource_dict)
