0065462f46fde890ae7db1ead19238a57f6e1d68
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-03 17:23:05 +0200

    Simplified results panel architecture

--- src/blackwall/panels/search/results.py
+++ src/blackwall/panels/search/results.py
@@ -15,38 +15,44 @@ DATASET_COLUMNS = [
     ("Dataset", "UACC", "Owner", "Created"),
 ]
 
-class PanelResultsMixedType(VerticalScroll):
+class PanelResultsUsers(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Label("Users:")
-        yield DataTable(id="results_user_table",)
-        yield Label("Groups:")
-        yield DataTable(id="results_group_table")
-        yield Label("Dataset profiles:")
-        yield DataTable(id="results_dataset_table")
-        yield Label("General resources profiles:")
-        yield DataTable(id="results_resource_table")
+        yield DataTable(id="results_user_table")     
 
     def on_mount(self) -> None:
         user_table = self.query_exactly_one(selector="#results_user_table")
         user_table.zebra_stripes = True
-        user_table.add_columns(*USER_COLUMNS[0])
+        user_table.add_columns(*USER_COLUMNS[0])   
 
+class PanelResultsGroup(VerticalScroll):
+    def compose(self) -> ComposeResult:
+        yield Label("Groups:")
+        yield DataTable(id="results_group_table")    
+
+    def on_mount(self) -> None:
         group_table = self.query_exactly_one(selector="#results_group_table")
         group_table.zebra_stripes = True
         group_table.add_columns(*GROUP_COLUMNS[0])
 
+class PanelResultsDatasets(VerticalScroll):
+    def compose(self) -> ComposeResult:
+        yield Label("Dataset profiles:")
+        yield DataTable(id="results_dataset_table")        
+
+    def on_mount(self) -> None:
         dataset_table = self.query_exactly_one(selector="#results_dataset_table")
         dataset_table.zebra_stripes = True
         dataset_table.add_columns(*DATASET_COLUMNS[0])
 
-class PanelResultsSingleType(VerticalScroll):
+class PanelResultsResources(VerticalScroll):
     def compose(self) -> ComposeResult:
-        yield DataTable()
+        yield Label("General resources profiles:")
+        yield DataTable(id="results_dataset_table")        
 
-class PanelResultUser(VerticalScroll):
+class PanelResultsMixedType(VerticalScroll):
     def compose(self) -> ComposeResult:
-        yield DataTable()        
-
-class PanelResultDataset(VerticalScroll):
-    def compose(self) -> ComposeResult:
-        yield DataTable()        
+        yield PanelResultsUsers()
+        yield PanelResultsGroup()
+        yield PanelResultsDatasets()
+        yield PanelResultsResources()
