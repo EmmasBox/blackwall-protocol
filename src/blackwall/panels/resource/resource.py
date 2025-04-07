ec2ad0b0a4396b7b54c599d2ddbe2c6e757e33f1
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-07 21:15:37 +0200

    Fixed some bugs in the resource panel

--- src/blackwall/panels/resource/resource.py
+++ src/blackwall/panels/resource/resource.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from textual.reactive import reactive
 from textual.app import ComposeResult
 from textual.widgets import Input, Label, Button, Collapsible
@@ -50,6 +52,10 @@ class PanelResourceActionButtons(HorizontalGroup):
     async def action_delete(self):
         await self.app.run_action(self.delete_action,default_namespace=self.parent)
 
+@dataclass
+class ResourceInfo:
+    mode: PanelMode = PanelMode.create
+
 class PanelResource(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield PanelResourceClassName()
@@ -57,70 +63,70 @@ class PanelResource(VerticalScroll):
         yield PanelResourceSegments()
         yield PanelResourceActionButtons(save_action="save_resource_profile", delete_action="delete_resource_profile")
 
-        def action_save_resource_profile(self) -> None:
-            resource_profile_name = self.query_exactly_one(selector="#resource_profile_name").value
-            resource_profile_class = self.query_exactly_one(selector="#resource_profile_class").value
-            resource_profile_exists = resource.resource_profile_exists(resource=resource_profile_name,resource_class=resource_profile_class)
+    def action_save_resource_profile(self) -> None:
+        resource_profile_name = self.query_exactly_one(selector="#resource_profile_name").value
+        resource_profile_class = self.query_exactly_one(selector="#resource_profile_class").value
+        resource_profile_exists = resource.resource_profile_exists(resource=resource_profile_name,resource_class=resource_profile_class)
 
-            if resource_profile_exists:
-                operator = "alter"
+        if resource_profile_exists:
+            operator = "alter"
+        else:
+            operator = "add"
+
+        base_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.BaseResourceTraits)
+        kerb_segment = get_traits_from_input(operator,self, prefix="kerb", trait_cls=resource.KerbResourceTraits)
+        dlfdata_segment = get_traits_from_input(operator,self, prefix="dlfdata", trait_cls=resource.DLFDataResourceTraits)
+        eim_segment = get_traits_from_input(operator,self, prefix="eim", trait_cls=resource.EIMResourceTraits)
+        jes_segment = get_traits_from_input(operator,self, prefix="jes", trait_cls=resource.JESResourceTraits)
+        icsf_segment = get_traits_from_input(operator,self, prefix="icsf", trait_cls=resource.ICSFResourceTraits)
+        ictx_segment = get_traits_from_input(operator,self, prefix="ictx", trait_cls=resource.ICTXResourceTraits)
+        idtparms_segment = get_traits_from_input(operator,self, prefix="idtparms", trait_cls=resource.IDTPARMSResourceTraits)
+        session_segment = get_traits_from_input(operator,self, prefix="session", trait_cls=resource.SessionResourceTraits)
+        svfmr_segment = get_traits_from_input(operator,self, prefix="svfmr", trait_cls=resource.SVFMRResourceTraits)
+        stdata_segment = get_traits_from_input(operator,self, prefix="stdata", trait_cls=resource.STDATAResourceTraits)
+        proxy_segment = get_traits_from_input(operator,self, prefix="proxy", trait_cls=resource.ProxyResourceTraits)
+        mfapolicy_segment = get_traits_from_input(operator,self, prefix="mfapolicy", trait_cls=resource.MFAPolicyResourceTraits)
+        sigver_segment = get_traits_from_input(operator,self, prefix="sigver", trait_cls=resource.SIGVERResourceTraits)
+        tme_segment = get_traits_from_input(operator,self, prefix="tme", trait_cls=resource.TMEResourceTraits)
+        cdtinfo_segment = get_traits_from_input(operator,self, prefix="cdtinfo", trait_cls=resource.CDTINFOResourceTraits)
+        ssignon_segment = get_traits_from_input(operator,self, prefix="ssignon", trait_cls=resource.SSIGNONResourceTraits)
+        cfdef_segment = get_traits_from_input(operator,self, prefix="cfdef", trait_cls=resource.CfdefResourceTraits)
+
+        result = resource.update_resource_profile(
+            resource=resource_profile_name,
+            resource_class=resource_profile_class,
+            create=not resource_profile_exists,
+            base=base_segment,
+            kerb=kerb_segment,
+            cdtinfo=cdtinfo_segment,
+            dlfdata=dlfdata_segment,
+            eim=eim_segment,
+            jes=jes_segment,
+            icsf=icsf_segment,
+            ictx=ictx_segment,
+            idtparms=idtparms_segment,
+            session=session_segment,
+            svfmr=svfmr_segment,
+            stdata=stdata_segment,
+            proxy=proxy_segment,
+            mfapolicy=mfapolicy_segment,
+            sigver=sigver_segment,
+            tme=tme_segment,
+            ssignon=ssignon_segment,
+            cfdef=cfdef_segment
+            )
+        
+        if not resource_profile_exists:
+            if (result == 0 or result == 4):
+                self.notify(f"General resource profile {resource_profile_name} created, return code: {result}",severity="information")
+                #self.set_edit_mode()
             else:
-                operator = "add"
-
-            base_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.BaseResourceTraits)
-            kerb_segment = get_traits_from_input(operator,self, prefix="kerb", trait_cls=resource.KerbResourceTraits)
-            dlfdata_segment = get_traits_from_input(operator,self, prefix="dlfdata", trait_cls=resource.DLFDataResourceTraits)
-            eim_segment = get_traits_from_input(operator,self, prefix="eim", trait_cls=resource.EIMResourceTraits)
-            jes_segment = get_traits_from_input(operator,self, prefix="jes", trait_cls=resource.JESResourceTraits)
-            icsf_segment = get_traits_from_input(operator,self, prefix="icsf", trait_cls=resource.ICSFResourceTraits)
-            ictx_segment = get_traits_from_input(operator,self, prefix="ictx", trait_cls=resource.ICTXResourceTraits)
-            idtparms_segment = get_traits_from_input(operator,self, prefix="idtparms", trait_cls=resource.IDTPARMSResourceTraits)
-            session_segment = get_traits_from_input(operator,self, prefix="session", trait_cls=resource.SessionResourceTraits)
-            svfmr_segment = get_traits_from_input(operator,self, prefix="svfmr", trait_cls=resource.SVFMRResourceTraits)
-            stdata_segment = get_traits_from_input(operator,self, prefix="stdata", trait_cls=resource.STDATAResourceTraits)
-            proxy_segment = get_traits_from_input(operator,self, prefix="proxy", trait_cls=resource.ProxyResourceTraits)
-            mfapolicy_segment = get_traits_from_input(operator,self, prefix="mfapolicy", trait_cls=resource.MFAPolicyResourceTraits)
-            sigver_segment = get_traits_from_input(operator,self, prefix="sigver", trait_cls=resource.SIGVERResourceTraits)
-            tme_segment = get_traits_from_input(operator,self, prefix="tme", trait_cls=resource.TMEResourceTraits)
-            cdtinfo_segment = get_traits_from_input(operator,self, prefix="cdtinfo", trait_cls=resource.CDTINFOResourceTraits)
-            ssignon_segment = get_traits_from_input(operator,self, prefix="ssignon", trait_cls=resource.SSIGNONResourceTraits)
-            cfdef_segment = get_traits_from_input(operator,self, prefix="cfdef", trait_cls=resource.CfdefResourceTraits)
-
-            result = resource.update_resource_profile(
-                resource=resource_profile_name,
-                resource_class=resource_profile_class,
-                create=not resource_profile_exists,
-                base=base_segment,
-                kerb=kerb_segment,
-                cdtinfo=cdtinfo_segment,
-                dlfdata=dlfdata_segment,
-                eim=eim_segment,
-                jes=jes_segment,
-                icsf=icsf_segment,
-                ictx=ictx_segment,
-                idtparms=idtparms_segment,
-                session=session_segment,
-                svfmr=svfmr_segment,
-                stdata=stdata_segment,
-                proxy=proxy_segment,
-                mfapolicy=mfapolicy_segment,
-                sigver=sigver_segment,
-                tme=tme_segment,
-                ssignon=ssignon_segment,
-                cfdef=cfdef_segment
-                )
-            
-            if not resource_profile_exists:
-                if (result == 0 or result == 4):
-                    self.notify(f"General resource profile {resource_profile_name} created, return code: {result}",severity="information")
-                    #self.set_edit_mode()
-                else:
-                    self.notify(f"Unable to create general resource profile, return code: {result}",severity="error")
+                self.notify(f"Unable to create general resource profile, return code: {result}",severity="error")
+        else:
+            if (result == 0 or result == 4):
+                self.notify(f"General resource profile {resource_profile_name} updated, return code: {result}",severity="information")
             else:
-                if (result == 0 or result == 4):
-                    self.notify(f"General resource profile {resource_profile_name} updated, return code: {result}",severity="information")
-                else:
-                    self.notify(f"Unable to update general resource profile, return code: {result}",severity="error")
+                self.notify(f"Unable to update general resource profile, return code: {result}",severity="error")
 
-        def action_delete_resource_profile(self) -> None:
-            pass
+    def action_delete_resource_profile(self) -> None:
+        pass
