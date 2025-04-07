7ff424f74c141944939ae6e6daa6e119f965cc66
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-07 20:47:42 +0200

    Made resource panel functional

--- src/blackwall/panels/resource/resource.py
+++ src/blackwall/panels/resource/resource.py
@@ -1,6 +1,6 @@
 from textual.reactive import reactive
 from textual.app import ComposeResult
-from textual.widgets import Input, Label, Button, RadioButton, Collapsible
+from textual.widgets import Input, Label, Button, Collapsible
 from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 from textual.lazy import Lazy
 
@@ -9,14 +9,12 @@ from blackwall.panels.panel_mode import PanelMode
 
 from ..traits_ui import generate_trait_section, get_traits_from_input
 
-from blackwall.api import resource
-
 class PanelResourceClassName(VerticalGroup):
     def compose(self) -> ComposeResult:
         yield Label("Profile name:")
-        yield Input(max_length=255,classes="resource-name-field")
+        yield Input(max_length=255,id="resource_profile_name",classes="resource-name-field")
         yield Label("Class:")
-        yield Input(max_length=8,classes="class-field")
+        yield Input(max_length=8,id="resource_profile_class",classes="class-field")
 
 class PanelResourceInstallationData(VerticalGroup):
     def compose(self) -> ComposeResult:
@@ -36,7 +34,7 @@ class PanelResourceActionButtons(HorizontalGroup):
         delete_is_disabled = False
     else:
         delete_is_disabled = True
-        
+
     def __init__(self, save_action: str, delete_action: str):
         super().__init__()
         self.save_action = save_action
@@ -60,7 +58,70 @@ class PanelResource(VerticalScroll):
         yield PanelResourceActionButtons(save_action="save_resource_profile", delete_action="delete_resource_profile")
 
         def action_save_resource_profile(self) -> None:
-            pass
+            resource_profile_name = self.query_exactly_one(selector="#resource_profile_name").value
+            resource_profile_class = self.query_exactly_one(selector="#resource_profile_class").value
+            resource_profile_exists = resource.resource_profile_exists(resource=resource_profile_name,resource_class=resource_profile_class)
+
+            if resource_profile_exists:
+                operator = "alter"
+            else:
+                operator = "add"
+
+            base_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.BaseResourceTraits)
+            kerb_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.KerbResourceTraits)
+            dlfdata_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.DLFDataResourceTraits)
+            eim_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.EIMResourceTraits)
+            jes_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.JESResourceTraits)
+            icsf_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.ICSFResourceTraits)
+            ictx_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.ICTXResourceTraits)
+            idtparm_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.IDTPARMSResourceTraits)
+            session_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.SessionResourceTraits)
+            svfmr_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.SVFMRResourceTraits)
+            stdata_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.STDATAResourceTraits)
+            proxy_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.ProxyResourceTraits)
+            mfapolicy_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.MFAPolicyResourceTraits)
+            sigver_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.SIGVERResourceTraits)
+            cdtinfo_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.CDTINFOResourceTraits)
+            tme_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.TMEResourceTraits)
+            cdtinfo_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.CDTINFOResourceTraits)
+            ssignon_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.SSIGNONResourceTraits)
+            cfdef_segment = get_traits_from_input(operator,self, prefix="base", trait_cls=resource.CfdefResourceTraits)
+
+            result = resource.update_resource_profile(
+                resource=resource_profile_name,
+                resource_class=resource_profile_class,
+                create=not resource_profile_exists,
+                base=base_segment,
+                kerb=kerb_segment,
+                cdtinfo=cdtinfo_segment,
+                dlfdata=dlfdata_segment,
+                eim=eim_segment,
+                jes=jes_segment,
+                icsf=icsf_segment,
+                ictx=ictx_segment,
+                idtparms=idtparm_segment,
+                session=session_segment,
+                svfmr=svfmr_segment,
+                stdata=stdata_segment,
+                proxy=proxy_segment,
+                mfapolicy=mfapolicy_segment,
+                sigver=sigver_segment,
+                tme=tme_segment,
+                ssignon=ssignon_segment,
+                cfdef=cfdef_segment
+                )
+            
+            if not resource_profile_exists:
+                if (result == 0 or result == 4):
+                    self.notify(f"General resource profile {resource_profile_name} created, return code: {result}",severity="information")
+                    #self.set_edit_mode()
+                else:
+                    self.notify(f"Unable to create general resource profile, return code: {result}",severity="error")
+            else:
+                if (result == 0 or result == 4):
+                    self.notify(f"General resource profile {resource_profile_name} updated, return code: {result}",severity="information")
+                else:
+                    self.notify(f"Unable to update general resource profile, return code: {result}",severity="error")
 
         def action_delete_resource_profile(self) -> None:
             pass
