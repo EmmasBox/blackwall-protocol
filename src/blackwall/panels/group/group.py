0ffa55e93834c13116f6b1948297f4b6c1f0d638
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-07 21:50:59 +0200

    Added action buttons to the group panel

--- src/blackwall/panels/group/group.py
+++ src/blackwall/panels/group/group.py
@@ -14,6 +14,30 @@ class PanelName(VerticalGroup):
         yield Label("Group name:")
         yield Input(id="group_name",classes="field-short-generic ")
 
+class PanelGroupActionButtons(HorizontalGroup):
+    edit_mode: reactive[PanelMode] = reactive(PanelMode.create,recompose=True)
+
+    if edit_mode is True:
+        delete_is_disabled = False
+    else:
+        delete_is_disabled = True
+
+    def __init__(self, save_action: str, delete_action: str):
+        super().__init__()
+        self.save_action = save_action
+        self.delete_action = delete_action
+    
+    def compose(self) -> ComposeResult:
+        yield Button("Save",action="save",classes="action-button")
+        yield Button("Delete",action="delete",classes="action-button",disabled=self.delete_is_disabled)
+
+    async def action_save(self):
+        await self.app.run_action(self.save_action,default_namespace=self.parent)
+
+    async def action_delete(self):
+        await self.app.run_action(self.delete_action,default_namespace=self.parent)
+
 class PanelGroups(VerticalGroup):
     def compose(self) -> ComposeResult:
         yield PanelName()
+        yield PanelGroupActionButtons(save_action="",delete_action="")
