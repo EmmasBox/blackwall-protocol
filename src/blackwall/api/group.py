a18d94ee28d0aac97ae21560ecdcf93addc7e0a2
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-14 21:27:32 +0200

    Changed function name

--- src/blackwall/api/group.py
+++ src/blackwall/api/group.py
@@ -49,13 +49,13 @@ class TMEGroupTraits(TraitsBase):
 
 if racfu_enabled:
     #Group functions
-    def group_profile_exists(group: str) -> bool:
+    def group_exists(group: str) -> bool:
         """Checks if a group exists, returns true or false"""
         result = racfu({"operation": "extract", "admin_type": "group", "profile_name": group.upper()})
         return result.result["return_codes"]["racf_return_code"] == 0
     
     def get_group(group: str):
-        """Doesn't handle group profiles that don't exist, recommend using group_profile_exists() first"""
+        """Doesn't handle group profiles that don't exist, recommend using group_exists() first"""
         if racfu_enabled:
             result = racfu({"operation": "extract", "admin_type": "group", "profile_name": group.upper()})
             return result.result
