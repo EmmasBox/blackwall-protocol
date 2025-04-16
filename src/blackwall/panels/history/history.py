009c3c1b01b127a2a21cb098e72d402781e35522
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-16 22:42:34 +0200

    Removed more unused code

--- src/blackwall/panels/history/history.py
+++ src/blackwall/panels/history/history.py
@@ -5,15 +5,9 @@ from textual.app import ComposeResult
 from textual.widgets import Log
 from textual.containers import VerticalScroll
 
-from blackwall.messages import CommandHistory
-
 class PanelHistory(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Log(id="command_log")
-
-    @on(CommandHistory)
-    async def on_command_history(self, message: CommandHistory):
-        pass
 
 
 
