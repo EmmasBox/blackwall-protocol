bba4e99cc59b5435858bdb7ab098440d9b9480fa
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-18 11:34:58 +0200

    Added date format setting

--- src/blackwall/submit_command.py
+++ src/blackwall/submit_command.py
@@ -3,9 +3,19 @@ from datetime import datetime
 
 import subprocess
 
+from blackwall.settings import get_user_setting
+
 def generate_command_meta_header(command: str) -> str:
+    date_format = get_user_setting(section="commands",setting="date_format")
+
     now = datetime.now() # current date and time
-    date_time = now.strftime("date: %m/%d/%Y time: %H:%M:%S")
+    if (date_format == "dmy" or date_format is None):
+        date_time = now.strftime("date: %d/%m/%Y time: %H:%M:%S")
+    elif date_format == "ymd":
+        date_time = now.strftime("date: %Y/%m/%d time: %H:%M:%S")
+    elif date_format == "mdy":
+        date_time = now.strftime("date: %m/%d/%Y time: %H:%M:%S")
+    
     local_now = now.astimezone()
     local_tz = local_now.tzinfo
     if local_tz is not None:
