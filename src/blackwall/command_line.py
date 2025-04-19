a94bcf8aa6a7d5158f79bef62fcbb5ad25591354
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-19 14:41:39 +0200

    Fixed bug

--- src/blackwall/command_line.py
+++ src/blackwall/command_line.py
@@ -34,7 +34,8 @@ class CommandLine(HorizontalGroup):
     def action_cycle_up(self) -> None:
         command_line = self.query_exactly_one(selector="#cli")
         command_line.value = self.command_history_list[self.current_command_history_entry] # type: ignore
-        self.current_command_history_entry = self.current_command_history_entry + 1
+        if self.current_command_history_entry < len(self.command_history_list):
+            self.current_command_history_entry = self.current_command_history_entry + 1
 
     def action_cycle_down(self) -> None:
         command_line = self.query_exactly_one(selector="#cli")
