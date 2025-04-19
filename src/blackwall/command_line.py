272dcc9f62e12e0eae84aa90b8b17a938a6a0edc
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-19 14:34:57 +0200

    Command cycle test

--- src/blackwall/command_line.py
+++ src/blackwall/command_line.py
@@ -10,6 +10,13 @@ from blackwall.messages import SubmitCommand
 from blackwall.settings import get_user_setting
 
 class CommandLine(HorizontalGroup):
+    BINDINGS = [
+        ("up", "cycle_up", "Cycle previous executed commands"),
+        ("down", "cycle_down", "Cycle previous executed commands")
+    ]
+
+    command_history_list = []
+
     def compose(self) -> ComposeResult:
         yield Input(id="cli",max_length=250,placeholder="Submit a TSO/RACF command...",classes="commands",suggester=SuggestFromList(commands,case_sensitive=False),tooltip="Use this command field to submit TSO and RACF commands. You can view the output in the command history panel")
 
@@ -20,4 +27,13 @@ class CommandLine(HorizontalGroup):
         self.post_message(SubmitCommand(command))
         clear_on_submission = get_user_setting(section="commands",setting="clear_on_submission")
         if clear_on_submission is not False:
+            self.command_history_list.append(command)
             command_line.value = ""
+
+    def action_cycle_up(self) -> None:
+        command_line = self.query_exactly_one(selector="#cli")
+        pass
+
+    def action_cycle_down(self) -> None:
+        command_line = self.query_exactly_one(selector="#cli")
+        command_line.value = self.command_history_list[-1] # type: ignore
