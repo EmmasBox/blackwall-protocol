24eadbb6148a314f389d7da9c410700bd863fee1
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-20 17:15:48 +0200

    Optimized code

--- src/blackwall/panels/search/search.py
+++ src/blackwall/panels/search/search.py
@@ -22,7 +22,7 @@ class SearchSelector(HorizontalGroup):
 
 class SearchField(HorizontalGroup):
     def action_search(self) -> None:
-        search_query = self.query_exactly_one("#search_field",Input).value
+        search_query = self.get_child_by_id("#search_field",Input).value
         results = search_database_query_one(query=search_query, class_name=None,query_types=QueryType.all())
         self.post_message(OpenTab("Results",PanelResultsMixedType(results)))
 
