6bfa186e7a55daec2daaecbe4028e6b6335bc346
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-20 23:44:28 +0200

    Fixed type errors

--- src/blackwall/submit_command.py
+++ src/blackwall/submit_command.py
@@ -9,12 +9,12 @@ def generate_command_meta_header(command: str) -> str:
     date_format = get_user_setting(section="locale",setting="date_format")
 
     now = datetime.now() # current date and time
-    if (date_format == "dmy" or date_format is None):
-        date_time = now.strftime("date: %d/%m/%Y time: %H:%M:%S")
-    elif date_format == "ymd":
+    if date_format == "ymd":
         date_time = now.strftime("date: %Y/%m/%d time: %H:%M:%S")
     elif date_format == "mdy":
         date_time = now.strftime("date: %m/%d/%Y time: %H:%M:%S")
+    else:
+        date_time = now.strftime("date: %d/%m/%Y time: %H:%M:%S")
     
     local_now = now.astimezone()
     local_tz = local_now.tzinfo
@@ -25,7 +25,7 @@ def generate_command_meta_header(command: str) -> str:
     
     return f"""
     __________________________________________________________________________________________________
-    
+
     Command '{command}' 
     Executed on {date_time}
     __________________________________________________________________________________________________
