2df12a865aa2e33c14baa1b938838d1591761600
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-20 16:41:28 +0200

    Optimized code

--- src/blackwall/panels/errors/history.py
+++ src/blackwall/panels/errors/history.py
@@ -14,6 +14,6 @@ class PanelErrorHistory(VerticalScroll):
         self.write_to_log(self.app.command_output) # type: ignore
 
     def write_to_log(self, output: str):
-        log = self.query_one("#error_log",Log)
+        log = self.get_child_by_id("#error_log",Log)
         log.clear()
-        log.write(output)%
+        log.write(output)
