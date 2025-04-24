9bb7d385f033b829e15625d7f960c8e0cf751954
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-04-24 16:26:36 +0200

    Fixed bugs

--- src/blackwall/panels/search/search.py
+++ src/blackwall/panels/search/search.py
@@ -71,52 +71,50 @@ class PanelSearch(VerticalScroll):
 
                 if 'profile' in user_dict and 'tso' in user_dict['profile']:
                     new_user_panel.user_info.tso_traits = user.TSOUserTraits.from_dict(prefix="tso",source=user_dict["profile"]["tso"])
-                    #if new_user_panel.tso_traits is None:
-                        #self.action_notify("test")
 
-                if "omvs" in user_dict:
+                if 'profile' in user_dict and 'omvs' in user_dict['profile']:
                     new_user_panel.user_info.omvs_traits = user.OMVSUserTraits.from_dict(prefix="omvs",source=user_dict["profile"]["omvs"])
 
-                if "nds" in user_dict:
+                if 'profile' in user_dict and 'nds' in user_dict['profile']:
                     new_user_panel.user_info.nds_traits = user.NDSUserTraits.from_dict(prefix="nds",source=user_dict["profile"]["nds"])
 
-                if "cics" in user_dict:
+                if 'profile' in user_dict and 'cics' in user_dict['profile']:
                     new_user_panel.user_info.cics_traits = user.CICSUserTraits.from_dict(prefix="cics",source=user_dict["profile"]["cics"])
 
-                if "netview" in user_dict:
+                if 'profile' in user_dict and 'netview' in user_dict['profile']:
                     new_user_panel.user_info.netview_traits = user.NetviewUserTraits.from_dict(prefix="netview",source=user_dict["profile"]["netview"])
 
-                if "mfa" in user_dict:
+                if 'profile' in user_dict and 'mfa' in user_dict['profile']:
                     new_user_panel.user_info.mfa_traits = user.MfaUserTraits.from_dict(prefix="mfa",source=user_dict["profile"]["mfa"])
                 
-                if "eim" in user_dict:
+                if 'profile' in user_dict and 'eim' in user_dict['profile']:
                     new_user_panel.user_info.eim_traits = user.EIMUserTraits.from_dict(prefix="eim",source=user_dict["profile"]["eim"])
 
-                if "workattr" in user_dict:
+                if 'profile' in user_dict and 'workattr' in user_dict['profile']:
                     new_user_panel.user_info.workattr_traits = user.WorkattrUserTraits.from_dict(prefix="workattr",source=user_dict["profile"]["workattr"])
                 
-                if "ovm" in user_dict:
+                if 'profile' in user_dict and 'ovm' in user_dict['profile']:
                     new_user_panel.user_info.ovm_traits = user.OvmUserTraits.from_dict(prefix="ovm",source=user_dict["profile"]["ovm"])
 
-                if "dce" in user_dict:
+                if 'profile' in user_dict and 'dce' in user_dict['profile']:
                     new_user_panel.user_info.dce_traits = user.DCEUserTraits.from_dict(prefix="dce",source=user_dict["profile"]["dce"])
                 
-                if "dfp" in user_dict:
+                if 'profile' in user_dict and 'dfp' in user_dict['profile']:
                     new_user_panel.user_info.dfp_traits = user.DFPUserTraits.from_dict(prefix="dfp",source=user_dict["profile"]["dfp"])
 
-                if "operparm" in user_dict:
+                if 'profile' in user_dict and 'operparm' in user_dict['profile']:
                     new_user_panel.user_info.operparm_traits = user.OperparmUserTraits.from_dict(prefix="operparm",source=user_dict["profile"]["operparm"])
 
-                if "proxy" in user_dict:
+                if 'profile' in user_dict and 'proxy' in user_dict['profile']:
                     new_user_panel.user_info.proxy_traits = user.ProxyUserTraits.from_dict(prefix="proxy",source=user_dict["profile"]["proxy"])
                 
-                if "lnotes" in user_dict:
+                if 'profile' in user_dict and 'lnotes' in user_dict['profile']:
                     new_user_panel.user_info.lnotes_traits = user.LnotesUserTraits.from_dict(prefix="lnotes",source=user_dict["profile"]["lnotes"])
 
-                if "language" in user_dict:
+                if 'profile' in user_dict and 'language' in user_dict['profile']:
                     new_user_panel.user_info.lang_traits = user.LanguageUserTraits.from_dict(prefix="language",source=user_dict["profile"]["language"])
 
-                if "kerb" in user_dict:
+                if 'profile' in user_dict and 'kerb' in user_dict['profile']:
                     new_user_panel.user_info.kerb_traits = user.KerbUserTraits.from_dict(prefix="kerb",source=user_dict["profile"]["kerb"])
                 
                 self.post_message(OpenTab(f"User: {search_query}",new_user_panel))
