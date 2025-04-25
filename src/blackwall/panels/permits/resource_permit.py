f7c160888db27c2041859acf9603e52caceda0de
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-04-25 18:23:21 +0200

    initial work on actions for buttons

--- src/blackwall/panels/permits/resource_permit.py
+++ src/blackwall/panels/permits/resource_permit.py
@@ -9,16 +9,30 @@ class PanelResourcePermitInfo(HorizontalGroup):
         yield Label("Use this panel to create, delete, and update permits for general resource profiles",classes="label-generic")
 
 class PanelResourcePermitSearchField(HorizontalGroup):
+    def __init__(self, search_action: str):
+        super().__init__()
+        self.search_action = search_action
+
     def compose(self) -> ComposeResult:
         yield Input(id="search_permit_class",placeholder="class...",classes="field-short-generic")
         yield Input(id="search_permit_profile",placeholder="profile name...",classes="search-field")    
-        yield Button(label="Search")
+        yield Button(label="Search",id="search_permit_button",action="search")
+
+    async def action_search(self):
+        await self.app.run_action(self.search_action,default_namespace=self.parent)
 
 class PanelResourcePermitCreate(HorizontalGroup):
+    def __init__(self, update_action: str):
+        super().__init__()
+        self.update_action = update_action
+    
     def compose(self) -> ComposeResult:
         yield Input(id="permit_receiver_field",placeholder="ID...",classes="field-short-generic", tooltip="User ID or group ID you want this permit change to affect")    
         yield Select([("NONE", "NONE"),("READ", "READ"),("EXECUTE", "EXECUTE"),("UPDATE", "UPDATE"),("CONTROL", "CONTROL"),("ALTER", "ALTER")],value="READ",classes="uacc-select",id="permit_access_selector")
-        yield Button(f"{get_emoji("💾")} Save",id="resource_permit_save")
+        yield Button(f"{get_emoji("💾")} Save",id="resource_permit_save",action="update")
+
+    async def action_create(self):
+        await self.app.run_action(self.update_action,default_namespace=self.parent)
 
 class PanelResourcePermitList(VerticalGroup):
     def compose(self) -> ComposeResult:
@@ -28,6 +42,12 @@ class PanelResourcePermitList(VerticalGroup):
 class PanelResourcePermit(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield PanelResourcePermitInfo()
-        yield PanelResourcePermitSearchField()
-        yield PanelResourcePermitCreate()
+        yield PanelResourcePermitSearchField(search_action="search")
+        yield PanelResourcePermitCreate(update_action="update")
         yield PanelResourcePermitList()
+
+    def action_search(self) -> None:
+        pass
+
+    def action_create(self) -> None:
+        pass
