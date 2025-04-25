a0f05fb2bd52c8fd48bd9eed250a6968f28653fb
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-04-25 17:47:51 +0200

    Added tool tip to the ID field

--- src/blackwall/panels/permits/resource_permit.py
+++ src/blackwall/panels/permits/resource_permit.py
@@ -10,7 +10,7 @@ class PanelResourcePermitSearchField(HorizontalGroup):
 
 class PanelResourcePermitCreate(HorizontalGroup):
     def compose(self) -> ComposeResult:
-        yield Input(id="permit_receiver_field",placeholder="ID...",classes="field-short-generic")    
+        yield Input(id="permit_receiver_field",placeholder="ID...",classes="field-short-generic", tooltip="User ID or group ID you want this permit change to affect")    
         yield Select([("NONE", "NONE"),("READ", "READ"),("EXECUTE", "EXECUTE"),("UPDATE", "UPDATE"),("CONTROL", "CONTROL"),("ALTER", "ALTER")],classes="uacc-select",id="permit_access_selector")
         yield Button(label="Save")
 
