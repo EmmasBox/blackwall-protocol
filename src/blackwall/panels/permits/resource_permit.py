f5142b665b5c0c8f59d92527fc9b62c92cda8525
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-04-25 17:50:52 +0200

    Added default value to access selector

--- src/blackwall/panels/permits/resource_permit.py
+++ src/blackwall/panels/permits/resource_permit.py
@@ -14,7 +14,7 @@ class PanelResourcePermitSearchField(HorizontalGroup):
 class PanelResourcePermitCreate(HorizontalGroup):
     def compose(self) -> ComposeResult:
         yield Input(id="permit_receiver_field",placeholder="ID...",classes="field-short-generic", tooltip="User ID or group ID you want this permit change to affect")    
-        yield Select([("NONE", "NONE"),("READ", "READ"),("EXECUTE", "EXECUTE"),("UPDATE", "UPDATE"),("CONTROL", "CONTROL"),("ALTER", "ALTER")],classes="uacc-select",id="permit_access_selector")
+        yield Select([("NONE", "NONE"),("READ", "READ"),("EXECUTE", "EXECUTE"),("UPDATE", "UPDATE"),("CONTROL", "CONTROL"),("ALTER", "ALTER")],value="READ",classes="uacc-select",id="permit_access_selector")
         yield Button(label="Save")
 
 class PanelResourcePermitList(VerticalGroup):
