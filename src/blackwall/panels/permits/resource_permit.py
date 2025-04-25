09ae510f6d214ee78f30b3e7dd79a5e12e56b6c2
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-04-25 17:57:37 +0200

    Added emoji and id to save button

--- src/blackwall/panels/permits/resource_permit.py
+++ src/blackwall/panels/permits/resource_permit.py
@@ -2,6 +2,8 @@ from textual.app import ComposeResult
 from textual.widgets import Input, Label, Button, Select, DataTable
 from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 
+from blackwall.emoji import get_emoji
+
 class PanelResourcePermitInfo(HorizontalGroup):
     def compose(self) -> ComposeResult:
         yield Label("Use this panel to create, delete, and update permits for general resource profiles")
@@ -15,7 +17,7 @@ class PanelResourcePermitCreate(HorizontalGroup):
     def compose(self) -> ComposeResult:
         yield Input(id="permit_receiver_field",placeholder="ID...",classes="field-short-generic", tooltip="User ID or group ID you want this permit change to affect")    
         yield Select([("NONE", "NONE"),("READ", "READ"),("EXECUTE", "EXECUTE"),("UPDATE", "UPDATE"),("CONTROL", "CONTROL"),("ALTER", "ALTER")],value="READ",classes="uacc-select",id="permit_access_selector")
-        yield Button(label="Save")
+        yield Button(f"{get_emoji("💾")} Save",id="resource_permit_save")
 
 class PanelResourcePermitList(VerticalGroup):
     def compose(self) -> ComposeResult:
