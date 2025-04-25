4ae28b513b38f1d648ffe07655179b03e11a883e
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-04-25 17:09:35 +0200

    Initial work on the permit API

---
+++ src/blackwall/api/permit.py
@@ -0,0 +1,50 @@
+#Permit API module for Blackwall Protocol, this wraps RACFU to increase ease of use and prevent updates from borking everything
+
+from dataclasses import dataclass, field
+from .traits_base import TraitsBase
+
+#Checks if RACFU can be imported
+import importlib.util
+
+racfu_enabled = importlib.util.find_spec('racfu')
+
+if racfu_enabled:
+    from racfu import racfu # type: ignore
+else:
+    print("##BLKWL_ERROR_2 Warning: could not find RACFU, entering lockdown mode")
+
+@dataclass
+class BasePermitTraits(TraitsBase):
+    access: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    model_profile_class: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    model_profile: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    model_profile_generic: bool | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    model_profile_volume: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    reset: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_partner_lu_name: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_console: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_jes: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_program: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_servauth: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_sms: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_service: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_system: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+    when_terminal: str | None = field(default=None,metadata={"allowed_in": {"alter"}})
+
+def update_dataset_permit(dataset: str, base: BasePermitTraits) -> int:
+    """Creates or updates a permit"""
+    if racfu_enabled:
+        traits = base.to_traits(prefix="base")
+
+        result = racfu({"operation": "alter", "admin_type": "permission", "data_set": dataset.upper(), "traits":  traits})
+        return result.result["return_codes"]["racf_return_code"] == 0
+    else:
+        return 8
+
+def delete_dataset_permit(dataset: str) -> int:
+    """Deletes a permit"""
+    if racfu_enabled:
+        result = racfu({"operation": "delete", "admin_type": "permission", "data_set": dataset.upper()})
+        return result.result["return_codes"]["racf_return_code"] == 0
+    else:
+        return 8
