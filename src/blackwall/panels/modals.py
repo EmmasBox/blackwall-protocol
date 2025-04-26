549de707e9273f9fd2b0a9d0083cc73e755488f3
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-26 22:53:28 +0200

    Renamed function

--- src/blackwall/panels/modals.py
+++ src/blackwall/panels/modals.py
@@ -3,7 +3,7 @@ from textual.widget import Widget
 
 from blackwall.screens.modal.modal import ModalScreen
 
-def generic_modal(self, modal_text: str, confirm_action: str, action_widget: Widget) -> None:
+def generic_confirmation_modal(self, modal_text: str, confirm_action: str, action_widget: Widget) -> None:
     modal_screen = ModalScreen(dialog_text=modal_text,confirm_action=confirm_action,action_widget=action_widget)
 
     self.app.push_screen(modal_screen)
--- src/blackwall/panels/users/user.py
+++ src/blackwall/panels/users/user.py
@@ -9,7 +9,7 @@ from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 from blackwall.api import user
 from blackwall.panels.panel_mode import PanelMode
 
-from blackwall.panels.modals import generic_modal
+from blackwall.panels.modals import generic_confirmation_modal
 from blackwall.screens.modal.modal import ModalScreen
 
 from ..traits_ui import generate_trait_section, get_traits_from_input, set_traits_in_input
@@ -266,7 +266,7 @@ class PanelUser(VerticalScroll):
                 self.notify(f"{message}, return code: {return_code}",severity="error")
 
     def action_delete_user(self) -> None:
-        generic_modal(self,modal_text="Are you sure you want to delete this user?",confirm_action="delete_user_api",action_widget=self)
+        generic_confirmation_modal(self,modal_text="Are you sure you want to delete this user?",confirm_action="delete_user_api",action_widget=self)
 
     def action_save_user(self) -> None:
         username = self.get_child_by_type(PanelUserName).get_child_by_id("username",Input).value
