5411006be2c5d6d7b12744bf3a270ebd01536e70
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-27 20:34:34 +0200

    Added CSS to backout panel

--- src/blackwall/panels/backout/backout.py
+++ src/blackwall/panels/backout/backout.py
@@ -6,7 +6,7 @@ from textual.widgets.option_list import Option
 
 class PanelBackout(VerticalScroll):
     def compose(self) -> ComposeResult:
-        yield Label("Backout changes")
+        yield Label("Backout changes",classes="backout-title")
         yield OptionList(
             Option("Create user 27/04/2025 at 20:30"),
             Option("Delete user 27/04/2025 at 20:30"),
