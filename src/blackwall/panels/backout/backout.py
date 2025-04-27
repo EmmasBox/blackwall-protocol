7a3236ac42434bf115c9bb76ba9c6b5e922b5efd
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-27 20:36:40 +0200

    Test

--- src/blackwall/panels/backout/backout.py
+++ src/blackwall/panels/backout/backout.py
@@ -3,12 +3,14 @@ from textual.app import ComposeResult
 from textual.widgets import Label, OptionList
 from textual.containers import VerticalScroll
 from textual.widgets.option_list import Option
+from rich.table import 
 
 class PanelBackout(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Label("Backout changes",classes="backout-title")
         yield OptionList(
             Option("Create user 27/04/2025 at 20:30"),
+            None,
             Option("Delete user 27/04/2025 at 20:30"),
             Option("Create resource profile 27/04/2025 at 20:30"),
             Option("Update dataset profile 27/04/2025 at 20:30")
