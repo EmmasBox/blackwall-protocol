ec6b0ed3830bf0b6b3a99e678b3ecfbd4636ec65
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-27 20:37:30 +0200

    Test

--- src/blackwall/panels/backout/backout.py
+++ src/blackwall/panels/backout/backout.py
@@ -11,6 +11,8 @@ class PanelBackout(VerticalScroll):
             Option("Create user 27/04/2025 at 20:30"),
             None,
             Option("Delete user 27/04/2025 at 20:30"),
+            None,
             Option("Create resource profile 27/04/2025 at 20:30"),
+            None,
             Option("Update dataset profile 27/04/2025 at 20:30")
         )
