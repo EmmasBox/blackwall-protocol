181407d2035c95831048a76b6b78a01037e6502c
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-28 16:12:52 +0200

    Changed to dialog to say which characters are allowed

--- src/blackwall/screens/modal/rvary.py
+++ src/blackwall/screens/modal/rvary.py
@@ -12,7 +12,7 @@ class RvaryScreen(Screen):
 
     def compose(self) -> ComposeResult:
         yield Grid(
-            Label("Change RVARY password for whole RACF database. Password must be up to 8 characters long. Only do this if you're absolutely sure you know what you're doing.", id="question"),
+            Label("Change RVARY password for whole RACF database. Password must be up to 8 characters long. Allowed characters: alphanumeric and @#$", id="question"),
             Input(id="rvary_password",restrict=r"([a-z]*[0-9]*[\@\#\$]*)",placeholder="enter password...",max_length=8,password=True),
             Input(id="rvary_password_confirm",restrict=r"([a-z]*[0-9]*[\@\#\$]*)",placeholder="confirm password...",max_length=8,password=True),
             Button("Cancel", variant="primary", id="cancel", classes="modal-buttons"),
