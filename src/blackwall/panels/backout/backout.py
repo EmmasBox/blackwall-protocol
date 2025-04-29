9781f905a79e5fa4b4037b46971a6bd771753e31
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-30 00:56:37 +0200

    Updated mockup
    Signed-off-by: Emma S. <64806882+EmmasBox@users.noreply.github.com>

--- src/blackwall/panels/backout/backout.py
+++ src/blackwall/panels/backout/backout.py
@@ -9,13 +9,13 @@ class PanelBackout(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Label("Backout changes",classes="backout-title")
         yield OptionList(
-            Option("Created user 'BLATEST1' 27/04/2025 at 20:30"),
+            Option("Created user 'BLATEST1' - 27/04/2025 at 20:30"),
             None,
-            Option("Deleted user 'BLATEST2' 27/04/2025 at 20:30"),
+            Option("Deleted user 'BLATEST2' - 27/04/2025 at 20:30"),
             None,
-            Option("Created resource 'BLATEST3.**' profile 27/04/2025 at 20:30"),
+            Option("Created resource 'BLATEST3.**' profile - 27/04/2025 at 20:30"),
             None,
-            Option("Updated dataset 'BLATEST7.**' profile 27/04/2025 at 20:30"),
+            Option("Updated dataset 'BLATEST7.**' profile - 27/04/2025 at 20:30"),
             None,
-            Option("Updated system options (SETROPTS) 27/04/2025 at 20:30"),
+            Option("Updated system options (SETROPTS) - 27/04/2025 at 20:30"),
         )
