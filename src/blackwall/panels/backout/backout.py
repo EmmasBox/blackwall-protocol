0c06cf0274acb656cad116f07248e81f3f46cc20
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-04-30 01:01:41 +0200

    Updated mockup
    Signed-off-by: Emma S. <64806882+EmmasBox@users.noreply.github.com>

--- src/blackwall/panels/backout/backout.py
+++ src/blackwall/panels/backout/backout.py
@@ -7,15 +7,15 @@ from textual.widgets.option_list import Option
 
 class PanelBackout(VerticalScroll):
     def compose(self) -> ComposeResult:
-        yield Label("Backout changes",classes="backout-title")
+        yield Label("Changes available for backout",classes="backout-title")
         yield OptionList(
-            Option("27/04/2025 at 20:30 - Created user 'BLATEST1'"),
+            Option("[27/04/2025 at 20:30] - Created user 'BLATEST1'"),
             None,
-            Option("27/04/2025 at 20:30 - Deleted user 'BLATEST2'"),
+            Option("[27/04/2025 at 20:30] - Deleted user 'BLATEST2'"),
             None,
-            Option("27/04/2025 at 20:30 - Created resource 'BLATEST3.**' profile"),
+            Option("[27/04/2025 at 20:30] - Created resource 'BLATEST3.**' profile"),
             None,
-            Option("27/04/2025 at 20:30 - Updated dataset 'BLATEST7.**' profile"),
+            Option("[27/04/2025 at 20:30] - Updated dataset 'BLATEST7.**' profile"),
             None,
-            Option("27/04/2025 at 20:30 - Updated system options (SETROPTS)"),
+            Option("[27/04/2025 at 20:30] - Updated system options (SETROPTS)"),
         )
