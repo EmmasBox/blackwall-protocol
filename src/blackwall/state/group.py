878e542403896bddede5603d264b653cc8bade8f
EmmasBox <64806882+EmmasBox@users.noreply.github.com>
2025-05-02 13:13:03 +0200

    Disabled code

--- src/blackwall/state/group.py
+++ src/blackwall/state/group.py
@@ -1,7 +1,7 @@
 
-from blackwall.api.group import update_group, BaseGroupTraits, DFPGroupTraits, OMVSGroupTraits, OVMGroupTraits, TMEGroupTraits
+#from blackwall.api.group import update_group, BaseGroupTraits, DFPGroupTraits, OMVSGroupTraits, OVMGroupTraits, TMEGroupTraits
 
-def update_group(group: str,create: bool, base: BaseGroupTraits, tme: TMEGroupTraits | None = None, omvs: OMVSGroupTraits | None = None, dfp: DFPGroupTraits | None = None, ovm: OVMGroupTraits | None = None):
+#def update_group(group: str,create: bool, base: BaseGroupTraits, tme: TMEGroupTraits | None = None, omvs: OMVSGroupTraits | None = None, dfp: DFPGroupTraits | None = None, ovm: OVMGroupTraits | None = None):
 
-def delete_group(group: str):
-    
+#def delete_group(group: str):
+    #pass
