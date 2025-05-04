65540b3593238fa187c98a4719d5c5458dff260a
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-05-04 16:55:11 +0200

    Added columns
    Signed-off-by: Emma S. <64806882+EmmasBox@users.noreply.github.com>

--- src/blackwall/panels/keyrings/keyrings.py
+++ src/blackwall/panels/keyrings/keyrings.py
@@ -6,6 +6,9 @@ from textual.widgets import DataTable, Input, Label
 
 from blackwall.api import keyrings
 
+CERTIFICATE_COLUMNS = [
+    ("DN", "Owner", "Issuer", "Key size", "Valid after", "Valid before"),
+]
 
 class PanelKeyringInfo(VerticalGroup):
     def compose(self) -> ComposeResult:
@@ -17,7 +20,13 @@ class PanelKeyringInfo(VerticalGroup):
 class PanelKeyringCertificates(VerticalGroup):
     def compose(self) -> ComposeResult:
         yield Label("certificates: ")
-        yield DataTable()
+        yield DataTable(id="certificates_table")
+
+    def on_mount(self) -> None:
+        certificates_table = self.get_child_by_id("certificates_table",DataTable)
+        certificates_table.zebra_stripes = True
+        certificates_table.add_columns(*CERTIFICATE_COLUMNS[0]) 
+
 
 @dataclass
 class KeyringInfo:
