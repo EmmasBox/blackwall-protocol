ce10e97be72c4914986a5324079388176cd54830
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-05-04 16:50:30 +0200

    Added ring_owner data
    Signed-off-by: Emma S. <64806882+EmmasBox@users.noreply.github.com>

--- src/blackwall/panels/keyrings/keyrings.py
+++ src/blackwall/panels/keyrings/keyrings.py
@@ -1,16 +1,18 @@
 from dataclasses import dataclass
 
 from textual.app import ComposeResult
-from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
+from textual.containers import VerticalGroup, VerticalScroll
 from textual.widgets import DataTable, Input, Label
 
 from blackwall.api import keyrings
 
 
-class PanelKeyringInfo(HorizontalGroup):
+class PanelKeyringInfo(VerticalGroup):
     def compose(self) -> ComposeResult:
         yield Label("Keyring: ")
-        yield Input(id="ring_name")
+        yield Input(id="ring_name",disabled=True)
+        yield Label("Owner: ")
+        yield Input(id="ring_owner",max_length=8,classes="field-short-generic",disabled=True)
 
 class PanelKeyringCertificates(VerticalGroup):
     def compose(self) -> ComposeResult:
