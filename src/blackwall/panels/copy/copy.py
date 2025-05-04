eef8b35af4cad23f2cf3c47ea408f9f90e6d55a0
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-05-04 22:49:48 +0200

    Initial work on copy tab
    Signed-off-by: Emma S. <64806882+EmmasBox@users.noreply.github.com>

---
+++ src/blackwall/panels/copy/copy.py
@@ -0,0 +1,40 @@
+from textual.app import ComposeResult
+from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
+from textual.widgets import Button, ContentSwitcher, Label
+
+
+class PanelCopyUser(HorizontalGroup):
+    def compose(self) -> ComposeResult:
+        yield Label("Copy user")
+
+class PanelCopyGroup(HorizontalGroup):
+    def compose(self) -> ComposeResult:
+        yield Label("Copy group")
+
+class PanelCopyDataset(HorizontalGroup):
+    def compose(self) -> ComposeResult:
+        yield Label("Copy dataset")
+
+class PanelCopyResource(HorizontalGroup):
+    def compose(self) -> ComposeResult:
+        yield Label("Copy resource")
+
+class PanelCopySwitcherButtons(HorizontalGroup):
+    def compose(self) -> ComposeResult:
+        yield Button(label="User")
+        yield Button(label="Group")
+        yield Button(label="Dataset profile")
+        yield Button(label="User")
+
+class PanelCopySwitcher(VerticalGroup):
+    def compose(self) -> ComposeResult:
+        yield PanelCopySwitcherButtons()
+        with ContentSwitcher():
+            yield PanelCopyUser()
+            yield PanelCopyGroup()
+            yield PanelCopyDataset()
+            yield PanelCopyResource()
+
+class PanelCopy(VerticalScroll):
+    def compose(self) -> ComposeResult:
+        yield PanelCopySwitcher()
