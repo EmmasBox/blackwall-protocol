4638e2547148b79f2032ed6a9091d31df0d58be9
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-05-30 22:22:10 +0200

    Enabled auto_gid
    Signed-off-by: Emma S. <64806882+EmmasBox@users.noreply.github.com>

--- src/blackwall/api/group.py
+++ src/blackwall/api/group.py
@@ -37,7 +37,7 @@ class DFPGroupTraits(TraitsBase):
 
 @dataclass
 class OMVSGroupTraits(TraitsBase):
-    auto_gid: str | None = field(default=None,metadata={"allowed_in": {"add","alter"},"invalid_values": {False}})
+    auto_gid: bool | None = field(default=None,metadata={"label": "Auto GID","allowed_in": {"add","alter"},"invalid_values": {False}})
     gid: int | None = field(default=None,metadata={"label": "GID", "input_args": {"max_length": 10,"classes": "field-medium-generic"}, "allowed_in": {"add","alter","extract"}})
     shared: str | None = field(default=None,metadata={"allowed_in": {"add","alter"},"invalid_values": {False}})
 
